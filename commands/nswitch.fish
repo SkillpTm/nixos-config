86f2d6efeab06af0e3143d686ae5ba2b38478790
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2026-03-08 17:39:05 +0100

    Switch: Generation 34 (idpl6qvwmx7mj87jpcc1wsrvqmpad5yv)

--- commands/nswitch.fish
+++ commands/nswitch.fish
@@ -1,11 +1,22 @@
+set PURPLE "\033[1;35m"
+set RESET "\033[0m"
+
 cd $HOME/nix-config
 git add .
-git diff --color=always
-sudo nixos-rebuild switch --flake .#$hostname --show-trace --no-reexec
-set GEN (nixos-rebuild list-generations | awk '$NF=="True" {print $1}' )
-set HASH (basename (readlink /run/current-system) | cut -d- -f1)
-git commit -m "Switch: Generation $GEN ($HASH)"
-set PREV_GEN (math $GEN - 1)
-nvd diff /nix/var/nix/profiles/system-$PREV_GEN-link /nix/var/nix/profiles/system
+git --no-pager diff --staged --color=always
 
-echo "Switch: Generation $GEN ($HASH)"
+set OLD_GEN (nixos-rebuild list-generations | awk '$NF=="True" {print $1}')
+sudo nixos-rebuild switch --flake .#$hostname --show-trace --no-reexec
+set NEW_GEN (nixos-rebuild list-generations | awk '$NF=="True" {print $1}' )
+
+if test $NEW_GEN -eq $OLD_GEN
+	git reset > /dev/null
+    echo -e "$PURPLE""No functional changes.""$RESET"
+    exit 0
+end
+
+set HASH (basename (readlink /run/current-system) | cut -d- -f1)
+git commit -m "Switch: Generation $NEW_GEN ($HASH)"
+
+nvd diff /nix/var/nix/profiles/system-$OLD_GEN-link /nix/var/nix/profiles/system
+echo -e "$PURPLE""Switch: Generation $NEW_GEN ($HASH)""$RESET"
--- commands/nupdate.fish
+++ commands/nupdate.fish
@@ -1,11 +1,24 @@
+set PURPLE "\033[1;35m"
+set RESET "\033[0m"
+
 cd $HOME/nix-config
 nix flake update
-git reset && git add flake.lock
-sudo nixos-rebuild switch --flake .#$hostname --show-trace --no-reexec
-set GEN (nixos-rebuild list-generations | awk '$NF=="True" {print $1}' )
-set HASH (basename (readlink /run/current-system) | cut -d- -f1)
-git commit -m "Update: Generation $GEN ($HASH)"
-set PREV_GEN (math $GEN - 1)
-nvd diff /nix/var/nix/profiles/system-$PREV_GEN-link /nix/var/nix/profiles/system
+git reset > /dev/null
+git add flake.lock
+git --no-pager diff --staged --color=always
 
-echo "Update: Generation $GEN ($HASH)"
+set OLD_GEN (nixos-rebuild list-generations | awk '$NF=="True" {print $1}')
+sudo nixos-rebuild switch --flake .#$hostname --show-trace --no-reexec
+set NEW_GEN (nixos-rebuild list-generations | awk '$NF=="True" {print $1}' )
+
+if test $NEW_GEN -eq $OLD_GEN
+	git reset > /dev/null
+    echo -e "$PURPLE""No functional changes.""$RESET"
+    exit 0
+end
+
+set HASH (basename (readlink /run/current-system) | cut -d- -f1)
+git commit -m "Update: Generation $NEW_GEN ($HASH)"
+
+nvd diff /nix/var/nix/profiles/system-$OLD_GEN-link /nix/var/nix/profiles/system
+echo -e "$PURPLE""Update: Generation $NEW_GEN ($HASH)""$RESET"
