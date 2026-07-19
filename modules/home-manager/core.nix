13eca9da02c9b429b9bb773b54177ef06ff4c2bb
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2026-07-19 23:50:42 +0200

    Switch: Generation 62 (gk0321kqdb545x1m17vxpszgm5shbaa1)

--- modules/home-manager/core.nix
+++ modules/home-manager/core.nix
@@ -9,11 +9,62 @@
 		users.${me} = { ... }: {
 			home.stateVersion = nixosVersion;
 			home.packages = [ pkgs.nerd-fonts.meslo-lg ];
+
 			imports = [
 				./fish/fish.nix
 				./plasma/plasma.nix
 				./vscode/vscode.nix
 			];
+
+			xdg = {
+				configFile = {
+					"autostart/discord-canary.desktop".source = "${pkgs.discord-canary}/share/applications/discord-canary.desktop";
+					"autostart/tutanota-desktop.desktop".source = "${pkgs.tutanota-desktop}/share/applications/tutanota-desktop.desktop";
+				};
+
+				dataFile = {
+					"wallpapers/Cheshire.jpg".source = ../../assets/wallpapers/Cheshire.jpg;
+					"wallpapers/Ryujin.jpeg".source = ../../assets/wallpapers/Ryujin.jpeg;
+					"wallpapers/Stage.jpeg".source = ../../assets/wallpapers/Stage.jpeg;
+				};
+
+				mimeApps = {
+					enable = true;
+					defaultApplications = {
+					"text/html" = "naver-whale.desktop";
+					"x-scheme-handler/http" = "naver-whale.desktop";
+					"x-scheme-handler/https" = "naver-whale.desktop";
+					"x-scheme-handler/about" = "naver-whale.desktop";
+					"x-scheme-handler/unknown" = "naver-whale.desktop";
+
+					"x-scheme-handler/mailto" = "tuta-desktop.desktop";
+
+					"image/jpeg" = "org.kde.gwenview.desktop";
+					"image/png" = "org.kde.gwenview.desktop";
+					"image/gif" = "org.kde.gwenview.desktop";
+					"image/webp" = "org.kde.gwenview.desktop";
+
+					"audio/mpeg" = "mpv.desktop";
+					"audio/x-wav" = "mpv.desktop";
+					"audio/flac" = "mpv.desktop";
+					"video/mp4" = "mpv.desktop";
+					"video/x-matroska" = "mpv.desktop";
+					"video/webm" = "mpv.desktop";
+
+					"text/plain" = "code.desktop";
+
+					"application/pdf" = "naver-whale.desktop";
+
+					"inode/directory" = "org.kde.dolphin.desktop";
+
+					"application/zip" = "org.kde.ark.desktop";
+					"application/x-tar" = "org.kde.ark.desktop";
+					"application/x-compressed-tar" = "org.kde.ark.desktop";
+
+					"x-scheme-handler/geo" = "openstreetmap.desktop";
+					};
+				};
+			};
 		};
 	};
 }
--- modules/home-manager/plasma/plasma.nix
+++ modules/home-manager/plasma/plasma.nix
@@ -8,5 +8,113 @@
 	programs.plasma = {
 		enable = true;
 		input.keyboard.options = [ "caps:none" "lv3:caps_switch" ];
+
+		configFile = {
+			kactivitymanagerdrc.Plugins."org.kde.ActivityManager.ResourceScoringEnabled" = false;
+			klipperrc.General.MaxClipItems = 2048;
+			kwinrc.Effect-overview.BorderActivate = 9;
+			kwinrc.Effect-shakecursor.Magnification = 10;
+			kwinrc.Wayland.EnablePrimarySelection = false;
+			PlasmaUserFeedback.Global.FeedbackLevel = 64;
+
+			kdeglobals = {
+				KDE.AnimationDurationFactor = 0.0;
+
+				General = {
+					TerminalApplication = "kitty";
+					TerminalService = "kitty.desktop";
+				};
+			};
+
+			plasma-localerc.Formats = {
+				LANG = "en_US.UTF-8";
+				LC_ADDRESS = "de_DE.UTF-8";
+				LC_IDENTIFICATION = "de_DE.UTF-8";
+				LC_MEASUREMENT = "de_DE.UTF-8";
+				LC_MONETARY = "de_DE.UTF-8";
+				LC_NAME = "de_DE.UTF-8";
+				LC_NUMERIC = "de_DE.UTF-8";
+				LC_PAPER = "de_DE.UTF-8";
+				LC_TELEPHONE = "de_DE.UTF-8";
+				LC_TIME = "de_DE.UTF-8";
+			};
+		};
+
+		kscreenlocker = {
+			autoLock = false;
+			timeout = 0;
+		};
+
+		panels = [
+			{
+				location = "bottom";
+				height = 44;
+
+				widgets = [
+					{
+						kickoff = {
+							icon = "nix-snowflake";
+						};
+					}
+
+					"org.kde.plasma.pager"
+
+					{
+						iconTasks = {
+							launchers = [
+								"applications:kitty.desktop"
+								"applications:org.kde.dolphin.desktop"
+								"applications:code.desktop"
+								"applications:discord-canary.desktop"
+								"applications:naver-whale.desktop"
+							];
+						};
+					}
+
+					"org.kde.plasma.panelspacer"
+
+					{
+						systemTray = {
+							items = {
+								shown = [
+									"tutanota-desktop"
+									"discord-canary"
+									"org.kde.plasma.clipboard"
+									"org.kde.plasma.volume"
+									"org.kde.plasma.networkmanagement"
+								];
+								
+								hidden = [
+									"org.kde.plasma.battery"
+									"org.kde.plasma.devicenotifier"
+									"org.kde.plasma.displayconfiguration"
+									"org.kde.plasma.notifications"
+									"org.kde.plasma.weather"
+								];
+							};
+						};
+					}
+
+					"org.kde.plasma.digitalclock"
+					"org.kde.plasma.showdesktop"
+				];
+			}
+		];
+
+		powerdevil.AC = {
+			autoSuspend.action = "nothing";
+			dimDisplay.enable = false;
+			powerButtonAction = "shutDown";
+			turnOffDisplay.idleTimeout = "never";
+		};
+
+		workspace = {
+			theme = "breeze-dark";
+
+			cursor = {
+				theme = "We10XOS-cursors";
+				size = 24;
+			};
+		};
 	};
 }
