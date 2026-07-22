3489b78c8a45a53321ae2dff5cf7819861f9f03f
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2026-07-23 00:39:55 +0200

    Switch: Generation 99 (mynvk6fvyl20s6mmdsn9s46cin6l8na9)

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -1,19 +1,23 @@
 { me, pkgs, ... }:
 
 {
-	home-manager.users.${me} = { ... }: {
-		xdg = {
-			configFile = {
-				"autostart/discord-canary.desktop".source = "${pkgs.discord-canary}/share/applications/discord-canary.desktop";
-				"autostart/spotify.desktop".source = "${pkgs.spotify}/share/applications/spotify.desktop";
-				"autostart/tutanota-desktop.desktop".source = "${pkgs.tutanota-desktop}/share/applications/tutanota-desktop.desktop";
-			};
+	home-manager.users.${me} = { lib, ... }: {
+		home.activation.copyWallpapers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+			mkdir -p $HOME/.local/share/wallpapers
 
-			dataFile = {
-				"wallpapers/Cheshire.jpg".source = ../../assets/wallpapers/Cheshire.jpg;
-				"wallpapers/Ryujin.jpeg".source = ../../assets/wallpapers/Ryujin.jpeg;
-				"wallpapers/Stage.jpeg".source = ../../assets/wallpapers/Stage.jpeg;
-			};
+			cp -f ${../../assets/wallpapers/Cheshire.jpg} $HOME/.local/share/wallpapers/Cheshire.jpg
+			cp -f ${../../assets/wallpapers/Ryujin.jpeg} $HOME/.local/share/wallpapers/Ryujin.jpeg
+			cp -f ${../../assets/wallpapers/Stage.jpeg} $HOME/.local/share/wallpapers/Stage.jpeg
+
+			chmod u+w $HOME/.local/share/wallpapers/Cheshire.jpg
+			chmod u+w $HOME/.local/share/wallpapers/Ryujin.jpeg
+			chmod u+w $HOME/.local/share/wallpapers/Stage.jpeg
+		'';
+
+		xdg.configFile = {
+			"autostart/discord-canary.desktop".source = "${pkgs.discord-canary}/share/applications/discord-canary.desktop";
+			"autostart/spotify.desktop".source = "${pkgs.spotify}/share/applications/spotify.desktop";
+			"autostart/tutanota-desktop.desktop".source = "${pkgs.tutanota-desktop}/share/applications/tutanota-desktop.desktop";
 		};
 	};
 }
