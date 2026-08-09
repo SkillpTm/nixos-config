14fe5a6863577e97907008b5ee357bde04ac4fb4
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2026-08-09 23:27:18 +0200

    Switch: desktop Generation 127 (v785vpws88ixn4lmdg882xqyc5zyzbpj)

--- modules/custom/whale/naver-whale.nix
+++ modules/custom/whale/naver-whale.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 let
 	hash = builtins.replaceStrings ["\n" "\r"] ["" ""] (builtins.readFile ./naver-whale-hash.txt);
@@ -8,7 +8,7 @@ let
 		version = "latest";
 
 		src = pkgs.fetchurl {
-		name = "naver-whale-${builtins.substring 0 8 hash}.deb";
+			name = "naver-whale-${builtins.substring 0 8 hash}.deb";
 			url = "https://installer-whale.pstatic.net/downloads/installers/naver-whale-stable_amd64.deb";
 			sha256 = hash;
 		};
@@ -27,6 +27,11 @@ let
 			substituteInPlace $out/share/applications/*.desktop \
 				--replace "/usr/bin/naver-whale-stable" "naver-whale" \
 				--replace "/usr/bin/naver-whale" "naver-whale"
+
+			substituteInPlace $out/share/gnome-control-center/default-apps/naver-whale.xml \
+				--replace "/opt/naver" "$out/opt/naver" || true
+			substituteInPlace $out/share/menu/naver-whale.menu \
+				--replace "/opt/naver" "$out/opt/naver" || true
 		'';
 	};
 
@@ -37,55 +42,70 @@ pkgs.buildFHSEnv {
 	targetPkgs = pkgs: with pkgs; [
 		whale-deb
 
-		# nix alien dependencies
-		alsa-lib-with-plugins.out
-		at-spi2-atk.out
-		cairo.out
+		alsa-lib-with-plugins
+		at-spi2-core
+		cairo
+		coreutils
 		cups.lib
+		curl
 		dbus.lib
-		expat.out
-		glib.out
-		libgbm.out
-		libx11.out
-		libxcb.out
-		libxcomposite.out
-		libxdamage.out
-		libxext.out
-		libxfixes.out
-		libxkbcommon.out
-		libxrandr.out
-		nspr.out
-		nss_latest.out
-		pango.out
-		systemdLibs.out
-
-		# libs required to make hardware acceleration possible
-		libglvnd
+		expat
+		freetype
+		fontconfig
+		glib
+		libdrm
+		libgbm
 		libGL
+		libglvnd
+		libpulseaudio
+		libva
+		libX11
+		libxcb
+		libXcomposite
+		libXdamage
+		libXext
+		libXfixes
+		libxkbcommon
+		libXrandr
 		mesa
+		nspr
+		nss
+		pango
+		pipewire
+		udev
 		vulkan-loader
 		wayland
-		libdrm
-		curl
-		libva
+		xdg-utils
 	];
 
 	runScript = pkgs.writeScript "naver-whale-wrapper" ''
 	#!/usr/bin/env bash
 		exec ${whale-deb}/opt/naver/whale/whale \
 			--enable-gpu \
-			--ozone-platform=wayland \
-			--use-gl=angle \
-			--use-angle=vulkan \
-			--enable-features=Vulkan,VaapiVideoDecoder \
+			--ozone-platform-hint=auto \
+			--enable-features=VaapiVideoDecoder,VaapiVideoEncoder,WaylandWindowDecorations,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE \
 			--disable-gpu-memory-buffer-video-frames \
 			"$@"
 	'';
 
 	extraInstallCommands = ''
-		mkdir -p $out/share/applications $out/share/icons/hicolor/32x32/apps $out/share/icons/hicolor/256x256/apps
+		mkdir -p $out/share/applications
 		ln -s ${whale-deb}/share/applications/*.desktop $out/share/applications/
-		ln -s ${whale-deb}/opt/naver/whale/product_logo_32.png $out/share/icons/hicolor/32x32/apps/naver-whale.png
-		ln -s ${whale-deb}/opt/naver/whale/product_logo_256.png $out/share/icons/hicolor/256x256/apps/naver-whale.png
+
+		for icon in 16 24 32 48 64 128 256; do
+			mkdir -p $out/share/icons/hicolor/''${icon}x''${icon}/apps
+			ln -s ${whale-deb}/opt/naver/whale/product_logo_$icon.png $out/share/icons/hicolor/''${icon}x''${icon}/apps/naver-whale.png
+		done
+
+		mkdir -p $out/opt/naver/whale
+		ln -sf ${pkgs.xdg-utils}/bin/xdg-settings $out/opt/naver/whale/xdg-settings
+		ln -sf ${pkgs.xdg-utils}/bin/xdg-mime $out/opt/naver/whale/xdg-mime
 	'';
+
+	meta = with pkgs.lib; {
+		description = "Web browser by NAVER";
+		homepage = "https://whale.naver.com";
+		platforms = [ "x86_64-linux" ];
+		mainProgram = "naver-whale";
+	};
 }
