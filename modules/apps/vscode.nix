e6a680b8903905fce670cc6bc7d54085adce091a
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2026-09-06 16:12:29 +0200

    Switch: desktop Generation 152 (g5xgmb3k4gm0gchm7s418bww16vldniq)

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -44,7 +44,7 @@ in
 
 			programs.vscode = {
 				enable = true;
-				package = pkgs.unstable.vscode;
+				package = pkgs.vscode-pinned.vscode;
 				profiles.default.extensions = with pkgs.vscode-extensions; [
 					christian-kohler.path-intellisense
 					dbaeumer.vscode-eslint
